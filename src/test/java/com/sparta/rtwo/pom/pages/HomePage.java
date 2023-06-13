dd78305d037176ee5eec75868f419338f2049490
tessthott <tessthott@gmail.com>
2023-06-13 16:31:54 +0100

    version 1.0 - tessa-added HomePage

---
+++ src/test/java/com/sparta/rtwo/pom/pages/HomePage.java
@@ -0,0 +1,45 @@
+package com.sparta.rtwo.pom.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+
+    private final WebDriver webDriver;
+
+    private By loginLink = new By.ByLinkText(" Signup / Login");
+    private By productLink = new By.ByLinkText(" Products");
+    private By cartLink = new By.ByLinkText(" Cart");
+    private By addToCartButton = new By.ByLinkText("Add to cart");
+    public HomePage(WebDriver webDriver){
+     this.webDriver = webDriver;
+     goToHomePage();
+    }
+
+    private void goToHomePage(){
+        webDriver.get("https://automationexercise.com/");
+    }
+
+    private void goToLoginPage(){
+        webDriver.findElement(loginLink).click();
+        // write return
+    }
+
+    private void goToProductPage(){
+        webDriver.findElement(productLink).click();
+        // write return
+    }
+
+    private void goToCartPage(){
+        webDriver.findElement(cartLink).click();
+        // write return
+    }
+
+    private void addToCart(){
+        webDriver.findElement(addToCartButton).click();
+        // write return
+    }
+
+
+
+}
