70a70600ea06a3c56db219e0df28f0b6d5c2e029
maria <mariamar_@hotmail.gr>
2023-06-14 14:57:15 +0100

    created AccountCreatedPage class and added the rest of the tests on MyStepdefs

--- src/test/java/com/sparta/rtwo/Stepdefs/MyStepdefs.java
+++ src/test/java/com/sparta/rtwo/Stepdefs/MyStepdefs.java
@@ -1,5 +1,6 @@
 package com.sparta.rtwo.Stepdefs;
 
+import com.sparta.rtwo.pom.pages.AccountCreatedPage;
 import com.sparta.rtwo.pom.pages.HomePage;
 import com.sparta.rtwo.pom.pages.LoginPage;
 import com.sparta.rtwo.pom.pages.SignupPage;
@@ -20,6 +21,7 @@ public class MyStepdefs {
     private HomePage homePage;
     private LoginPage loginPage;
     private SignupPage signupPage;
+    private AccountCreatedPage accountCreatedPage;
 
 
     private  static ChromeDriverService service;
@@ -52,85 +54,112 @@ public class MyStepdefs {
 
     @Given("I am on the login page")
     public void iAmOnTheLoginPage() {
+        homePage = new HomePage(webDriver);
+        loginPage = homePage.goToLoginPage();
     }
 
     @When("I enter a name")
     public void iEnterAName() {
+        loginPage.enterName("name");
     }
 
     @And("email address")
     public void emailAddress() {
+        loginPage.enterEmail("name@domain.com");
     }
 
     @And("click signup")
     public void clickSignup() {
+        signupPage = loginPage.clickSignUp();
     }
 
     @Then("I am directed to the signup page")
     public void iAmDirectedToTheSignupPage() {
+        Assertions.assertEquals("https://automationexercise.com/signup", signupPage.getUrl());
     }
 
     @And("The name filed contain the name entered")
     public void theNameFiledContainTheNameEntered() {
+        Assertions.assertEquals("name",signupPage.getName());
+
     }
 
     @And("The email field contain the email entered")
     public void theEmailFieldContainTheEmailEntered() {
+        Assertions.assertEquals("name@domain.com", signupPage.getEmail());
     }
 
     @Given("I am on the signup page")
     public void iAmOnTheSignupPage() {
+        homePage = new HomePage(webDriver);
+        loginPage = homePage.goToLoginPage();
+        loginPage.enterName("name");
+        loginPage.enterEmail("name@domain.com");
+        signupPage = loginPage.clickSignUp();
     }
 
     @When("I select my title")
     public void iSelectMyTitle() {
+        signupPage.selectTitle();
     }
 
     @And("enter a password")
     public void enterAPassword() {
+        signupPage.enterPassword("password");
     }
 
     @And("select my date of birth")
     public void selectMyDateOfBirth() {
+        signupPage.dateDropDownSelect("1","January","2000");
     }
 
     @And("enter my first name")
     public void enterMyFirstName() {
+        signupPage.enterFirstName("firstname");
     }
 
     @And("enter my last name")
     public void enterMyLastName() {
+        signupPage.enterLastName("lastname");
     }
 
     @And("enter my address")
     public void enterMyAddress() {
+        signupPage.enterAddress("123 lane");
     }
 
     @And("select my country")
     public void selectMyCountry() {
+        signupPage.countryDropDownSelect("Singapore");
     }
 
     @And("enter my state")
     public void enterMyState() {
+        signupPage.enterState("State");
     }
 
     @And("enter my city")
     public void enterMyCity() {
+        signupPage.enterCity("London");
     }
 
     @And("enter my zip code")
     public void enterMyZipCode() {
+        signupPage.enterZipcode("123");
     }
 
     @And("enter my phone number")
     public void enterMyPhoneNumber() {
+        signupPage.enterPhone("123456");
     }
 
     @And("click create account")
     public void clickCreateAccount() {
+        signupPage.clickCreateAccount();
     }
 
     @Then("I will be  redirected to a page account created")
     public void iWillBeRedirectedToAPageAccountCreated() {
+        Assertions.assertEquals("https://automationexercise.com/account_created",accountCreatedPage.getUrl());
     }
 }
--- src/test/java/com/sparta/rtwo/pom/pages/LoginPage.java
+++ src/test/java/com/sparta/rtwo/pom/pages/LoginPage.java
@@ -8,6 +8,10 @@ public class LoginPage {
     private final By usernameBy = new By.ByName("email");
     private final By passwordBy = new By.ByName("password");
     private final By loginButtonBy = new By. ByLinkText("Login");
+    private final By name = new By.ByName("name");
+    private final By email = new By.ByXPath("//*[@id=\"form\"]/div/div/div[3]/div/form/input[3]");
+    //    private final By signUpButton = new By.ByLinkText("Signup");
+    private final By signUpButton = new By.ByXPath("//*[@id=\"form\"]/div/div/div[3]/div/form/button");
 
     WebDriver webDriver;
     public LoginPage(WebDriver webdriver){
@@ -15,6 +19,16 @@ public class LoginPage {
     }
     public String getUrl(){
         return webDriver.getCurrentUrl();
+    }
+    public void enterName(String nameInput){
+        webDriver.findElement(name).sendKeys(nameInput);
+    }
+    public void enterEmail(String emailInput){
+        webDriver.findElement(email).sendKeys(emailInput);
+    }
+    public SignupPage clickSignUp(){
+        webDriver.findElement(signUpButton).click();
+        return new SignupPage(webDriver);
     }
     public void enterUserName(String username) {
         var userNameBox = webDriver.findElement(usernameBy);
